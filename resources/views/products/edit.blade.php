508c9b843b18f47d3a111c5ad02f8ad88e72d1d3
znl.haq <muhammadrobitzainulhaq@gmail.com>
2024-10-30 04:23:45 +0700

    Projek RPL

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -25,19 +25,14 @@
                 <input type="number" step="0.01" id="price" name="price" value="{{ $product->price }}" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             </div>
             <div class="mb-4">
-<<<<<<< HEAD
+
                 <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Descriptionnn</label>
                 <textarea id="description" name="description" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">{{ $product->description }}</textarea>
             </div>
+            
             <div class="flex items-center justify-between">
-                <input type="submit" value="Update Product" class="bg-yellow-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-=======
-                <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Description</label>
-                <textarea id="description" name="description" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">{{ $product->description }}</textarea>
-            </div>
-            <div class="flex items-center justify-between">
+
                 <input type="submit" value="Update Product" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
->>>>>>> d29199a (Projek Laravel RPL)
             </div>
         </form>
     </div>
